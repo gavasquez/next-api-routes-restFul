9439713fb9aca9352511786b89344f9a039a0726
gavasquez <gustavovasquez15@gmail.com>
2024-06-22 11:49:15 -0500

    Paginacion simple

---
+++ src/app/api/todos/route.ts
@@ -0,0 +1,26 @@
+import prisma from '@/lib/prisma';
+import { NextResponse, NextRequest } from 'next/server'
+
+export async function GET(req: Request) {
+
+  const { searchParams } = new URL(req.url)
+  const take = Number(searchParams.get('take') ?? '10');
+  const skip = Number(searchParams.get('skip') ?? '0');
+  if(isNaN(take)) {
+    return NextResponse.json({message: 'Take tiene que ser un número'}, { status: 400});
+  }
+
+  if(isNaN(skip)) {
+    return NextResponse.json({message: 'Skip tiene que ser un número'}, { status: 400});
+  }
+
+  const todos = await prisma.todo.findMany({
+    take,
+    skip
+  });
+
+  return NextResponse.json({
+    todos,
+  });
+
+ }
