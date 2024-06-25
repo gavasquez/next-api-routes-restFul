a97db6d8441d32b35f1085cd34c6f25d9f8e93ff
gavasquez <gustavovasquez15@gmail.com>
2024-06-25 11:57:42 -0500

    Fin de la seccion-9

---
+++ src/app/api/todos/[id]/route.ts
@@ -0,0 +1,59 @@
+import prisma from '@/lib/prisma';
+import { Todo } from '@prisma/client';
+import { NextResponse } from 'next/server'
+import * as yup from 'yup';
+
+interface Segments {
+  params: {
+    id: string,
+  }
+}
+
+const getTodo =async (id: string): Promise<Todo | null> => {
+  const todo = await prisma.todo.findFirst({where: { id: id}});
+  return todo;
+}
+
+export async function GET(request: Request, {params}: Segments) { 
+
+  const { id } = params;
+
+  const todo = await getTodo(id);
+
+  if(!todo) return NextResponse.json({message: `Todo with id: ${id} not found`}, { status: 404}); 
+
+  return NextResponse.json(todo);
+}
+
+const putSchema = yup.object({
+  complete: yup.bool().optional().default(false),
+  description: yup.string().optional(),
+});
+
+
+export async function PUT(request: Request, {params}: Segments) { 
+
+  const { id } = params;
+
+  const todo = await getTodo(id);
+
+  if(!todo) return NextResponse.json({message: `Todo with id: ${id} not found`}, { status: 404});
+
+  try {
+  
+    const { complete, description } = await putSchema.validate(await request.json());
+  
+    const updatedTodo = await prisma.todo.update({
+      where: { id },
+      data:  { complete, description },
+    });
+  
+    return NextResponse.json(updatedTodo);
+
+  } catch (error) {
+    return NextResponse.json( { message: error }, { status: 400 });
+
+  }
+
+}
+
--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,5 +1,6 @@
 import prisma from '@/lib/prisma';
 import { NextResponse, NextRequest } from 'next/server'
+import * as yup from 'yup';
 
 export async function GET(req: Request) {
 
@@ -23,4 +24,24 @@ export async function GET(req: Request) {
     todos,
   });
 
- }
+}
+
+const postSchema = yup.object({
+  description: yup.string().required(),
+  complete: yup.boolean().optional().default(false), // todo: Mostrar algo interesante
+});
+
+export async function POST(request: Request) { 
+
+  try {
+    const { complete, description } = await postSchema.validate(await request.json());
+    
+    const todo = await prisma.todo.create({ data: { complete, description } });   
+
+    return NextResponse.json(todo);
+
+  } catch (error) {
+    return NextResponse.json(error, { status: 400 }); 
+  }
+
+}
